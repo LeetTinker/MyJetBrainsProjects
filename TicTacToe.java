6f6beb93acdf085653343b2d49fb6b11168a40ad
Keshav Mittal <117512651+LeetTinker@users.noreply.github.com>
2023-01-12 18:04:44 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,6 @@
 package tictactoe;
 import java.util.Scanner;
-public class Main {
+public class TicTacToe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Boolean x3 = false;
